279adcdfc7b962f20a79bad9495477d09108a09c
Starup <billionaire@starup.(none)>
2013-07-20 06:46:30 -0400

    updated web.js for upload

--- web.js
+++ web.js
@@ -1,5 +1,30 @@
 var fs  = require('fs');
 
+//var bufcontent = fs.readFileSync('index.html');
+//var len = bufcontent.length;
+
+
+//console.log(bufcontent.toString('utf8',0,len));
+
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+
+  //response.send('Hello World21!');
+   var bufcontent = fs.readFileSync('index.html');
+   var len = bufcontent.length;
+   response.send(bufcontent.toString('utf8',0,len)); 
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+
+
 /*
 fs.readFile('index.html','utf-8',function (err,data){
     if (err) throw err;
@@ -8,26 +33,8 @@ fs.readFile('index.html','utf-8',function (err,data){
 */
 
 //var bufcontent  = new Buffer();
+/*
 var bufcontent = fs.readFileSync('index.html');
 var len = bufcontent.length;
 
-console.log(bufcontent.toString('utf8',0,len));
-
-
-/*
-var express = require('express');
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World21!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
-*/
-
-
+console.log(bufcontent.toString('utf8',0,len));*/
